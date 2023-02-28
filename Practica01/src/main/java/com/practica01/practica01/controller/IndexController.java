fd8c6d6d3cbc1c58652718bd664e92b9202e904a
manul <manul@DESKTOP-KCC0TG9>
2023-02-27 23:11:50 -0600

    tabla y el objeto estado

--- Practica01/src/main/java/com/practica01/practica01/controller/IndexController.java
+++ Practica01/src/main/java/com/practica01/practica01/controller/IndexController.java
@@ -1,5 +1,8 @@
 package com.practica01.practica01.controller;
 
+import com.practica01.practica01.domain.Estado;
+import java.util.Arrays;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,6 +18,12 @@ public class IndexController {
 
     @GetMapping("/")
     public String page(Model model) {
+
+        Estado estado = new Estado("estado", "capital", "poblacion", "costas");
+        Estado estado2 = new Estado("estado2", "capital2", "poblacion2", "costas2");
+//        model.addAttribute("estado", estado);
+        List<Estado> estados = Arrays.asList(estado, estado2);
+        model.addAttribute("estados", estados);
         return "index";
     }
 
--- Practica01/src/main/java/com/practica01/practica01/domain/Estado.java
+++ Practica01/src/main/java/com/practica01/practica01/domain/Estado.java
@@ -1,3 +1,7 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package com.practica01.practica01.domain;
 
 import lombok.Data;
@@ -7,21 +11,21 @@ import lombok.Data;
  * @author manul
  */
 @Data
-public class estados {
+public class Estado {
+
     String estado;
     String capital;
     String poblacion;
     String costas;
 
-    public estados() {
+    public Estado() {
     }
 
-    public estados(String estado, String capital, String poblacion, String costas) {
+    public Estado(String estado, String capital, String poblacion, String costas) {
         this.estado = estado;
         this.capital = capital;
         this.poblacion = poblacion;
         this.costas = costas;
     }
-    
-    
+
 }
