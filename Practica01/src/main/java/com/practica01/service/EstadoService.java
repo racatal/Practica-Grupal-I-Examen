a8b653e86433beec59a16bd21cbd8923f478d2e8
racatal <catvalra@gmail.com>
2023-02-28 17:27:17 -0600

    Intento de levantamiento localhost // Tag de transactional y Transactional readOnly

--- Practica01/src/main/java/com/practica01/service/EstadoService.java
+++ Practica01/src/main/java/com/practica01/service/EstadoService.java
@@ -3,7 +3,6 @@ package com.practica01.service;
 import com.practica01.practica01.domain.Estado;
 import java.util.List;
 
-
 public interface EstadoService {
     
     public List<Estado> getEstados();
--- Practica01/src/main/java/com/practica01/service/EstadoServiceImpl.java
+++ Practica01/src/main/java/com/practica01/service/EstadoServiceImpl.java
@@ -5,6 +5,7 @@ import com.practica01.practica01.domain.Estado;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class EstadoServiceImpl implements EstadoService{
@@ -13,21 +14,25 @@ public class EstadoServiceImpl implements EstadoService{
     EstadoDao estadoDao;
 
     @Override
+    @Transactional(readOnly = true)
     public List<Estado> getEstados() {
         return (List<Estado>)estadoDao.findAll();
         }
 
     @Override
+    @Transactional(readOnly = true)
     public Estado getEstado(Estado estado) {
         return estadoDao.findById(estado.getIdEstado()).orElse(null);
         }
 
     @Override
+    @Transactional
     public void save(Estado estado) {
         estadoDao.save(estado);
        }
 
     @Override
+    @Transactional
     public void delete(Estado estado) {
         estadoDao.deleteById(estado.getIdEstado());
         }
